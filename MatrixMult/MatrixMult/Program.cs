c33e62fa2e09d5c8e390714a1bc8249dc02d3354
Montecarlos <montecarlos@gmx.net>
2016-09-06 22:36:59 +0200

    tested MatrixMult with 2x1 and 1x2 matrix multiplication. Results are ok for the special cases tested. Returning an empty matrix for illegal dimensining of the input matrices seems to work, too

--- MatrixMult/MatrixMult/Program.cs
+++ MatrixMult/MatrixMult/Program.cs
@@ -4,12 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+/*
+ *          ..    x   .... = ....
+ *          ..        ....   ....
+ *          ..               ....
+ *          ..               ....   
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+ 
 namespace MatrixMult
 {
   
     class testMMult
     {
-        public double[,] MatrixMult(double[,] a, double[,] b)
+        static public double[,] MatrixMult(ref double[,] a, ref double[,] b)
         {
             int rows = a.GetLength(0), cols = a.GetLength(1);
 
@@ -18,14 +35,14 @@ namespace MatrixMult
                 return new double[0, 0];
             }
             double[,] m = new double[rows, rows];
-            for(uint r=0; r<rows;++r)
+            for(uint r = 0; r < rows; ++r)
             {
-                for (uint c = 0; c < cols; ++c)
+                for (uint c = 0; c < rows; ++c)
                 {
                     double t = 0;
                     for (uint i = 0; i < cols; ++i)
                     { 
-                        t += a[r, i] * b[i, r];
+                        t += a[r, i] * b[i, c];
                     }
                     m[r, c] = t;
                 }
@@ -33,12 +50,68 @@ namespace MatrixMult
             return m;
         }
 
+        static void PrintMatrix(ref double[,] m)
+        {
+            int ccol = Console.CursorLeft;
+
+            for (uint j = 0; j < m.GetLength(0); ++j)
+            {
+                for (uint i = 0; i < m.GetLength(1); ++i)
+                {
+                    Console.Write("{0,5:f2}", m[j, i]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+                Console.CursorLeft = ccol;
+            }
+            Console.CursorLeft = ccol+m.GetLength(1)*5;
+        }
+
         static void Main(string[] args)
         {
             double[,] a = { { 1 }, { 2 } };
             double[,] b = { { 1, 2 } };
-            double[,] c = MatrixMult(a, b);
+            double[,] c = MatrixMult(ref a, ref b);
+            int crow = Console.CursorTop;
 
+            PrintMatrix(ref a);
+            Console.CursorTop = crow;
+            Console.Write("*");
+            PrintMatrix(ref b);
+            Console.CursorTop = crow;
+            Console.Write("=");
+            PrintMatrix(ref c);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            crow = Console.CursorTop;
+
+            PrintMatrix(ref b);
+            Console.CursorTop = crow;
+            Console.Write("*");
+            PrintMatrix(ref a);
+            Console.CursorTop = crow;
+            Console.Write("=");
+            c = MatrixMult(ref b, ref a);
+            PrintMatrix(ref c);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            crow = Console.CursorTop;
+
+            PrintMatrix(ref a);
+            Console.CursorTop = crow;
+            Console.Write("*");
+            PrintMatrix(ref a);
+            Console.CursorTop = crow;
+            Console.Write("=");
+            c = MatrixMult(ref a, ref a);
+            PrintMatrix(ref c);
+
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
