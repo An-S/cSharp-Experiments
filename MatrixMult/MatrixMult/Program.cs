f6451581a88011b95093059ecca850f4bd365328
mc78 <mc78@outlook.de>
2016-09-06 10:53:18 +0200

    added second c# project (Matrix mul) however git repository is still named after first project (counter)

---
+++ MatrixMult/MatrixMult/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixMult
+{
+  
+    class testMMult
+    {
+        double[,] MatrixMult(double[,] a, double[,] b)
+        {
+            if (a.GetLength(0) != b.GetLength(1))
+            {
+                return new double[0, 0];
+            }
+            double[,] m = new double[a.GetLength(1) * b.GetLength(0), a.GetLength(1) * b.GetLength(0)];
+            foreach(double s in a[1])
+            {
+                foreach(double c in b[0])
+                {
+
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
