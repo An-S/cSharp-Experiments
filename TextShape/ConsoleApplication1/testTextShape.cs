a77ada648370f335b9388733b46208bdc1c365cd
mc78 <mc78@outlook.de>
2016-09-12 10:07:37 +0200

    added Textshape project to c#experiments repo

---
+++ TextShape/ConsoleApplication1/testTextShape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextShape
+{
+    class testTextShape
+    {
+        static void Main(string[] args)
+        {
+            Shape shp = new Shape(5, 10);
+            shp.draw();
+            Console.WriteLine();
+            shp = new Shape(10, 10, 5, 1, -1);
+            shp.draw();
+            Console.WriteLine();
+            shp = new Shape(10, 1, 9, -1, 2);
+            shp.draw();
+            Console.WriteLine();
+            shp = new Shape(10, 19, 0, 1, -2);
+            shp.draw();
+        }
+    }
+}
