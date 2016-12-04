09ec6885c4958dafcb0f35d3483bc78b1dd6242b
mc78 <mc78@outlook.de>
2016-12-04 20:47:34 +0100

    added dirstat project files

---
+++ dirstat/dirstat/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: MyAcer
+ * Date: 04.12.2016
+ * Time: 13:18
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+
+class MainClass{
+	void prFile(FileInfo fl){
+		Console.WriteLine(fl.Name);
+	}
+	
+	static void Main(string[] args)
+	{
+		/*var myDirStat = dirstat.Create();
+		if (args.Length != dirstat.numArgs){
+			Console.WriteLine("Usage: {0} dir", myDirStat.ExeName);
+		}*/
+		
+		DirectoryInfo di = new DirectoryInfo("testdir");
+		//IEnumerable<DirectoryInfo> en = di.EnumerateDirectories();
+		
+		//printoutDirs(ref di, di.Name);
+
+		di = new DirectoryInfo("./");
+		//IEnumerable<DirectoryInfo> en = di.EnumerateDirectories();
+	
+		var fr = new FilesRunner(di, prFile);
+	}
+}
