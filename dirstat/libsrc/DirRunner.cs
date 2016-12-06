25806292ad5590a2241cb48a762c2d576cfc8356
mc78 <mc78@outlook.de>
2016-12-06 15:57:37 +0100

    added DirRunner similar to FileRunner.cs added before and after delegates to File and DirRunner

---
+++ dirstat/libsrc/DirRunner.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: MyAcer
+ * Date: 06.12.2016
+ * Time: 15:52
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+/// <summary>
+/// Description of Class1.
+/// </summary>
+public class DirRunner/*
+ * Created by SharpDevelop.
+ * User: MyAcer
+ * Date: 04.12.2016
+ * Time: 13:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+
+
+public class DirRunner:dirstat
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	static void emptyFnc(FileInfo f){
+
+	}
+
+	public DirRunner(ref DirectoryInfo di, dirDelegate dg, dirDelegate before, dirDelegate after){
+		if (di.Exists)
+		{
+			//Console.WriteLine(path);
+			if (before) before(di);
+			foreach (DirectoryInfo d in di.EnumerateDirectories())
+			{
+				//path2 = Path.Combine(path,f.Name);
+				//Console.WriteLine(path2);
+				dg(d);
+			}
+			if (after) after(di);
+		}else
+		{
+			Console.WriteLine("Dir -{0}- does not exist.", di.Name);
+		}
+	}
+
+
+}
+
+
+
+{
+	public Class1()
+	{
+	}
+}
---
+++ dirstat/libsrc/FileRunner.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: MyAcer
+ * Date: 04.12.2016
+ * Time: 13:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+
+
+public class FilesRunner:dirstat
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	static void emptyFnc(FileInfo f){
+
+	}
+
+	public FilesRunner(ref DirectoryInfo di, fileDelegate dg, fileDelegate before, fileDelegate after){
+		if (di.Exists)
+		{
+			//Console.WriteLine(path);
+			if (before) before(di);
+			foreach (FileInfo f in di.EnumerateFiles())
+			{
+				//path2 = Path.Combine(path,f.Name);
+				//Console.WriteLine(path2);
+				dg(f);
+			}
+			if (after) after(di);
+		}else
+		{
+			Console.WriteLine("Dir -{0}- does not exist.", di.Name);
+		}
+	}
+
+
+}
+
+
