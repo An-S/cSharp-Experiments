e3729294a75d574f3f0af148745a03baee3066ee
mc78 <mc78@outlook.de>
2016-12-11 21:02:36 +0100

    added tests which checks exceptions for DirUtil.CreateDirInfo

---
+++ dirstat/tests/provideValidFilename.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+public class MainClass
+{
+    public static String ProvideValidFilename()
+    {
+        return Application.ExecutablePath;
+    }
+    static void Main(string[] args)
+    {
+    }
+
+}
--- dirstat/tests/utestFileExists.cs
+++ dirstat/tests/utestFileExists.cs
@@ -12,27 +12,63 @@ public class testFileExists
     
     public class testDirRunner
     {
-        [TestCase]
-        public void TestCreateDirInfo()
+        //define method with optional param.
+        //If not given it is auto populated with the current filename
+        /*public static String getFilename([CallerFilePath] string file = "")
         {
-            String dirname = "test";
-            DirUtil.CreateDirInfo di;
-            try
-            {
-                di = new DirUtil.CreateDirInfo(dirname);
-                Fail();
-            }
-            catch (IOException e)
-            {
-                Assert.AreEqual(dirname, di.DInfo.Name);
-                Assert.AreEqual(false, di.dirExists);
-            }
+            return Path.GetFileName(file);
+        }*/
+
+        private static DirUtil.CreateDirInfo getDir(string path)
+        {
+            var di = new DirUtil.CreateDirInfo(path);
+            return di;
+        }
+
+        [TestCase]
+        public void TestInitializationOfDirUtil_CreateDirInfo()
+        {
+            String dirname = MainClass.ProvideValidFilename();
+            var cdi = getDir(dirname);
+            var di = new DirectoryInfo(dirname);
+
+            Assert.AreEqual(cdi.dirExists, cdi.DInfo.Exists);
+            Assert.AreEqual(di, cdi.DInfo);
+        }
+
+        [TestCase]
+        public void TestCreateDirInfoThrowsNotOnValidPath()
+        {
+            //Some dirname unlikely to be found
+            String dirname = MainClass.ProvideValidFilename();
+            Console.WriteLine(dirname);
+            DirUtil.CreateDirInfo cdi;
+
+            Assert.DoesNotThrow(
+                delegate { cdi = getDir(dirname); }
+            );
+
+        }
+
+        [TestCase]
+        //[ExpectedException(typeof(FileNotFoundException))]
+        public void TestCreateDirInfoThrowsOnInvalidPath()
+        {
+            //Some dirname unlikely to be found
+            String dirname = "loiu324ghcb 34";
+            DirUtil.CreateDirInfo cdi;
+
+            Assert.Throws<FileNotFoundException>(
+                delegate { cdi = getDir(dirname);}
+            );
+            
         }
 
         [TestCase]
         public void TestFObjExists()
         {
-            var fi = new FileInfo("utestFileExists.dll");
+            var di = new DirectoryInfo(".");
+            var fi = new FileInfo(di.GetFiles()[0].Name);
             Assert.AreEqual(true, FileSystemHelper.checkIfFileSystemObjExists(fi));
             fi = new FileInfo("not a file");
             Assert.AreEqual(false, FileSystemHelper.checkIfFileSystemObjExists(fi));
